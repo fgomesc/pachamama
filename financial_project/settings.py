75ee9c9f0a349b5b6f541d50eac317b6cd1c38c4
Felipe Gomes <fgomesc0586@gmail.com>
2021-09-16 14:22:14 -0300

    Import project

--- financial_project/settings.py
+++ financial_project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', defaul=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
